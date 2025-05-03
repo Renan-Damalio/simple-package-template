4555e674de0c8fad7ab3f4f9ff5620dc5f920fa1
Renan-Damalio <renan.damalio@gmail.com>
2025-05-03 13:53:49 -0300

    __init__.py

---
+++ package_name/__init__.py
@@ -0,0 +1,11 @@
+"""
+Pacote: meu_pacote
+Descrição: Pacote Python contendo funcionalidades de file1_name e file2_name.
+"""
+
+__version__ = "0.1.0"
+
+from .file1_name import *  # ou funções específicas
+from .file2_name import *
+
+__all__ = []  # Adicione os nomes públicos dos módulos aqui, se quiser controlar importações globais
